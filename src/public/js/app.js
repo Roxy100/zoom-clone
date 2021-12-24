1d4ac1dd6519a7bafab851705fc7d6567395f6bd
Roxy100 <misoamy16@gmail.com>
2021-12-24 15:31:27 +0900

    <Message> using WebSocket Event

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,22 @@
 // socket은 서버로의 연결.
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+// <Message 받기>
+// connection이 open일 때
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+// message를 받을 때마다 내용을 출력하는 message.
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+});
+
+// 서버가 오프라인이 됐을 때
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -19,11 +19,25 @@ const server = http.createServer(app);
 // HTTP서버 위에 새로운 webSocket서버를 만들 수 있다. (server를 전달 수 있도록)
 const wss = new WebSocket.Server({ server });
 
-// socket은 서버와 브라우저사이의 연결. 즉. 연결된 브라우저.
-function handleConnection(socket) {
-  console.log(socket);
+function onSocketClose() {
+  console.log("Disconnected the Browser ❌");
 }
+
+function onSocketMessage(message) {
+  console.log(message.toString());
+}
+
+// socket은 서버와 브라우저사이의 연결. 즉. 연결된 브라우저.
 // on method는 event가 발동하는 거 기다림. backend에 연결된 사람의 정보를 제공해줌.
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+  // 브라우저가 연결되면,
+  console.log("Connected to Browser ✅");
+  // 브라우저가 꺼졌을 때를 위한 listener 등록(close)
+  socket.on("close", onSocketClose);
+  // 브라우저가 서버에 메세지를 보냈을 때를 위한 listener 등록(message)
+  socket.on("message", onSocketMessage);
+  // 브라우저에 메세지를 보내도록 만듬.
+  socket.send("hello!!");
+});
 
 server.listen(3000, handleListen);
