6ef259f63c5e4a4a419deae70d2543a89bb046ea
Roxy100 <misoamy16@gmail.com>
2021-12-24 10:32:50 +0900

    Setup Fontend

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug"); // pug로 view engine을 설정.
+app.set("views", __dirname + "/views"); // express에 template이 어디 있는지 지정.
+app.use("/public", express.static(__dirname + "/public")); // public url을 생성해서 유저에게 공개.
 
-app.listen(3000);
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
