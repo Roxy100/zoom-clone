ba6b6f26828b318b6521361c8e74e1b98daad6b2
Roxy100 <misoamy16@gmail.com>
2021-12-29 21:00:48 +0900

    Created to Show Media , MuteBtn , CameraBtn and Move them

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,58 @@
 // io는 자동적으로 back-end socketIO와 연결해주는 함수이다.
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+// <화면에 비디오 보여주기>
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getMedia();
+
+// <Mute 버튼 생성>
+function handleMuteClick() {
+  // 오디오Track 생성
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+
+// <Camera 버튼 생성>
+function handleCameraClick() {
+  // 비디오Track 생성
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
